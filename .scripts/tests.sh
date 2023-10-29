14aa72e0226efb2ba2c47dfafef78c8f64eec81a
Caio <c410.f3r@gmail.com>
2023-10-29 16:31:46 -0300

    Install rust-tools

--- .scripts/tests.sh
+++ .scripts/tests.sh
@@ -1,6 +1,8 @@
 set -euxo pipefail
 
-export rt='rust-tools --template you-rust'
+cargo install rust-tools --git https://github.com/c410-f3r/regular-crates
+
+rt='rust-tools --template you-rust'
 
 export CARGO_TARGET_DIR="$($rt target-dir)"
 export RUSTFLAGS="$($rt rust-flags)"
